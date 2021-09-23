0e55eaf95391647de39d3e155e62b1902991d813
Foysal Mahmud <dev.foysal@gmail.com>
2021-09-23 19:30:45 +0600

    modify home page

--- pages/index.js
+++ pages/index.js
@@ -7,44 +7,76 @@ import fetch from "isomorphic-unfetch";
 import Simpleslider from "../partials/home/homeslider";
 import HomeAbout from "../partials/home/aboutSection";
 import Homewhatsnew from "../partials/home/homeWhatsnew";
-class Index extends React.Component {
-	static async getInitialProps({ reduxStore, req }) {
-		let json = {};
+// class Index extends React.Component {
+// 	static async getInitialProps({ reduxStore, req }) {
+// 		let json = {};
 
-		if (req) {
-			// const token = req.cookies[variables.apiTokenKey];
-			const response = await fetch(variables.apiUrls.getHomeContent, {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-					// Authorization: "Bearer " + token
-				}
-			});
-			json = await response.json();
-		} else {
-			const response = await getRequest(variables.apiUrls.getHomeContent);
-			json = response;
-		}
+// 		if (req) {
+// 			// const token = req.cookies[variables.apiTokenKey];
+// 			const response = await fetch(variables.apiUrls.getHomeContent, {
+// 				method: "GET",
+// 				headers: {
+// 					"Content-Type": "application/json",
+// 					// Authorization: "Bearer " + token
+// 				}
+// 			});
+// 			json = await response.json();
+// 		} else {
+// 			const response = await getRequest(variables.apiUrls.getHomeContent);
+// 			json = response;
+// 		}
 
-		return { item: json, reduxStore: reduxStore };
+// 		return { item: json, reduxStore: reduxStore };
+// 	}
+
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {};
+// 	}
+
+// 	render() {
+// 		const { item } = this.props;
+// 		return (
+// 			<Layout title={"Home"} isHome={true}>
+// 				<Simpleslider data={item.data.sections.homeSlider.data} />
+// 				<HomeAbout data={item.data.sections.aboutSection} />
+// 				<Homewhatsnew data={item.data.sections.whatsNew} whatsnew={true} />
+// 			</Layout>
+// 		);
+// 	}
+// }
+
+
+const Index = ({item}) => {
+	return (
+		<Layout title={"Home"} isHome={true}>
+			<Simpleslider data={item.data.sections.homeSlider.data} />
+			<HomeAbout data={item.data.sections.aboutSection} />
+			<Homewhatsnew data={item.data.sections.whatsNew} whatsnew={true} />
+		</Layout>
+	);
+}
+
+
+Index.getInitialProps = async({ reduxStore, req }) => {
+	let json = {};
+
+	if (req) {
+		// const token = req.cookies[variables.apiTokenKey];
+		const response = await fetch(variables.apiUrls.getHomeContent, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				// Authorization: "Bearer " + token
+			}
+		});
+		json = await response.json();
+	} else {
+		const response = await getRequest(variables.apiUrls.getHomeContent);
+		json = response;
 	}
 
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-
-	render() {
-		const { item } = this.props;
-		// console.log(item);
-		return (
-			<Layout title={"Home"} isHome={true}>
-				<Simpleslider data={item.data.sections.homeSlider.data} />
-				<HomeAbout data={item.data.sections.aboutSection} />
-				<Homewhatsnew data={item.data.sections.whatsNew} whatsnew={true} />
-			</Layout>
-		);
-	}
+	return { item: json, reduxStore: reduxStore };
 }
 
 function mapStateToProps(state) {
